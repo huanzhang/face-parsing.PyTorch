af9f72d2ebeb14c165fe0a8fd0ad60feaf15c2aa
Zhang Huan <zhang_huan@yahoo.com>
2020-07-30 10:59:06 +0800

    rename demo.py to wipe_background.py

--- wipe_background.py
+++ wipe_background.py
@@ -69,7 +69,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
 
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
-    # net.cuda()
+    net.cuda()
     save_pth = osp.join('res/cp', cp)
     net.load_state_dict(torch.load(save_pth, map_location='cpu'))
     net.eval()
@@ -84,7 +84,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
             image = img.resize((512, 512), Image.BILINEAR)
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
-            # img = img.cuda()
+            img = img.cuda()
             out = net(img)[0]
             parsing = out.squeeze(0).cpu().numpy().argmax(0)
             #print(parsing)
